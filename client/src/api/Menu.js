bb9b17172ec883da9231a3ba6a715db5eb5aebd8
yunubuntu <744561873@qq.com>
2020-04-15 00:36:36 +0800

    菜单

--- client/src/api/Menu.js
+++ client/src/api/Menu.js
@@ -6,9 +6,12 @@ export default {
                 adapter: async (config) => {
                     return {
                         data: [
-                            { name: '用户管理', sub: '导航一', index: 'menu1', componentPath: 'components/UserManager.vue' },
-                            { name: '菜单二', sub: '导航二', group: '分组一', index: 'menu2' },
-                            { name: '菜单二', sub: '导航二', group: '分组二', index: 'menu4' },
+                            { name: '数据字典', sub: '基础信息',  index: 'menu11' },
+                            { name: '节假日', sub: '基础信息', index: 'menu12', componentPath: 'components/UserManager.vue' },
+                            { name: 'MI', sub: '基础信息',  index: 'menu13' },
+                            { name: '公式', sub: '基础信息',  index: 'menu14' },
+                            { name: '内部PO', sub: '接口数据', index: 'menu2' },
+                            { name: '市场排单', sub: '接口数据', index: 'menu4' },
                             { name: '菜单三', index: 'menu3' },
                             { name: '菜单五', group: '分组二', index: 'menu5' },
                         ],
